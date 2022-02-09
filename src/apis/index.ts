8df12fbd8e19aa72c33c7b1442e66b19026c31b8
Tomas Basham <tomasbasham@gmail.com>
2022-02-09 23:09:58 +0000

    Add StatusCake client code

---
+++ src/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ContactGroupsApi';
+export * from './LocationsApi';
+export * from './MaintenanceWindowsApi';
+export * from './PagespeedApi';
+export * from './SslApi';
+export * from './UptimeApi';
